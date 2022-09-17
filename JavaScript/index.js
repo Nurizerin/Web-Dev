47ff2e38e8479d5166a18628684fd218acc31a3a
Nurizerin <nurizerin1515@gmail.com>
2022-09-17 21:49:32 +0600

    Lecture-1 class added

--- JavaScript/index.js
+++
@@ -1,4 +0,0 @@
-console.log("hello from Nuri zerin")
-console.log("hello nuri");
-
-console.log(5+3)
--- app.js
+++
@@ -1,7 +0,0 @@
-let a = 5;
-let b = 6;
-function sum(x, y) {
-    sum = x + y;
-    return sum;
-}
-console.log(sum(a, b));
